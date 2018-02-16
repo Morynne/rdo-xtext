d6e694b1c24dbdc6546747a1030633874707d643
Morynne <osokol@gobuyfood.ru>
2018-02-16 18:49:54 +0300

    Add files via upload

---
+++ 11.0/start.bash
@@ -0,0 +1,1 @@
+./ethdcrminer64 -epool eth-eu1.nanopool.org:9999 -ewal 0x9908452f3e3ccbb5fba7c2acd2adf712a3da898b.Palit_Ready/logitech96@yandex.ru -epsw x -dcoin blake2s -dpool xvg.eu1.unimining.net:5770 -dwal D6EKWmQ22adhovstd17yg6TMCnk4YUKEd9 -dpsw x -mode 0 -ftime 10
